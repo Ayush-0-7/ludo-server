6d14f053eb831f05a8116792bd03a31d353ecf7c
Ayush <156231875+Ayush-0-7@users.noreply.github.com>
2025-09-09 12:36:08 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -5,15 +5,9 @@ import { createRequire } from 'module';
 let serviceAccount;
 
 // Check if the environment variable is available (for production/deployment)
-if (process.env.FIREBASE_SERVICE_ACCOUNT_BASE64) {
-    // Decode the Base64 string to get the JSON content
+// Decode the Base64 string to get the JSON content
     const decodedServiceAccount = Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT_BASE64, 'base64').toString('utf8');
     serviceAccount = JSON.parse(decodedServiceAccount);
-} else {
-    // Fallback for local development: read the file from disk
-    const require = createRequire(import.meta.url);
-    serviceAccount = require('./serviceAccountKey.json');
-}
 
 
 admin.initializeApp({
