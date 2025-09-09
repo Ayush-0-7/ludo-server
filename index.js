2fdaa7b778dc68924c23aa102b6892b0ca7dfc17
Ayush <156231875+Ayush-0-7@users.noreply.github.com>
2025-09-09 17:13:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -273,5 +273,5 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 4000;
+const PORT =4000;
 server.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
